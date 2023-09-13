66d7294335551dae6b06c5c5fef774a1b696a065
Aayush <aayushpatel25190@gmail.com>
2023-09-13 23:59:54 +0530

    Mobile Chat Screen

---
+++ lib/screens/mobile_chat_screen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/colors.dart';
+import 'package:whatsapp_ui/info.dart';
+import 'package:whatsapp_ui/widgets/chat_list.dart';
+
+class MobileChatScreen extends StatelessWidget {
+  // final int? index;
+  const MobileChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: appBarColor,
+        // ---> Back & Profile picture
+        // leading: Row(
+        //   children: [
+        //     IconButton(
+        //       onPressed: () {
+        //         Navigator.pop(context);
+        //       },
+        //       icon: const Icon(
+        //         Icons.arrow_back,
+        //       ),
+        //     ),
+        //     CircleAvatar(
+        //       backgroundImage: NetworkImage(
+        //         info[0]['profilePic'].toString(),
+        //       ),
+        //     ),
+        //   ],
+        // ),
+        title: Text(
+          info[0]['name'].toString(),
+        ),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.video_call),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.call),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          // Chat List
+          const Expanded(
+            child: ChatList(),
+          ),
+          // Text Input
+          TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: mobileChatBoxColor,
+              prefixIcon: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Icon(
+                  Icons.emoji_emotions,
+                  color: Colors.grey,
+                ),
+              ),
+              suffixIcon: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Icon(
+                      Icons.camera_alt,
+                      color: Colors.grey,
+                    ),
+                    Icon(
+                      Icons.attach_file,
+                      color: Colors.grey,
+                    ),
+                    Icon(
+                      Icons.money,
+                      color: Colors.grey,
+                    ),
+                  ],
+                ),
+              ),
+              hintText: "Type a message",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: const BorderSide(
+                  width: 0,
+                  style: BorderStyle.none,
+                ),
+              ),
+              contentPadding: const EdgeInsets.all(10),
+            ),
+          ),
+          // space heigh
+          const SizedBox(
+            height: 8,
+          ),
+        ],
+      ),
+    );
+  }
+}
